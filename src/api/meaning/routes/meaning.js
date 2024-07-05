123eeeff9759be039ad4ddbd281c772f54d8ab61
Tigran Yeranyan <tigran.e@purlin.com>
2024-07-05 12:28:10 +0400

    initial commit

---
+++ src/api/meaning/routes/meaning.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * meaning router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::meaning.meaning');
---
+++ src/api/meaning/services/meaning.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * meaning service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::meaning.meaning');
